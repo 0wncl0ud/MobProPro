cf369747da0c2ecb318cbe17622001ed92d02b14
Manuel Felber <manuel.felber@stud.hslu.ch>
2016-04-18 14:22:34 +0200

    init

---
+++ TeamSRMF/app/src/main/java/mobpro/hslu/ch/teamsrmf/MainActivity.java
@@ -0,0 +1,13 @@
+package mobpro.hslu.ch.teamsrmf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
