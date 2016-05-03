0da330bc22aee8cf82c7ca3b85fc173fd924b6a6
Manuel Felber <manuel.felber@stud.hslu.ch>
2016-05-03 19:36:18 +0200

    Json library hinzugefügt

---
+++ Server/src/ch/hslu/mobile/android/client/TCPClient.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hslu.mobile.android.client;
+
+/**
+ *
+ * @author Manuel
+ */
+import java.io.*;
+import java.net.*;
+
+class TCPClient {
+
+    public static void main(String argv[]) throws Exception {
+        String sentence;
+        String modifiedSentence;
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        Socket clientSocket = new Socket("localhost", 4711);
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        sentence = inFromUser.readLine();
+        outToServer.writeBytes(sentence + '\n');
+        modifiedSentence = inFromServer.readLine();
+        System.out.println("FROM SERVER: " + modifiedSentence);
+        clientSocket.close();
+    }
+}
--- Server/src/ch/hslu/mobile/android/server/socket/EchoReceiverThread.java
+++ Server/src/ch/hslu/mobile/android/server/socket/EchoReceiverThread.java
@@ -1,7 +1,12 @@
 package ch.hslu.mobile.android.server.socket;
 
+import java.io.FileReader;
 import java.io.IOException;
-
+import java.io.StringWriter;
+import java.util.Iterator;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONArray;
 /**
  * A receiver thread for a single socket. Returns received request strings (echo
  * behavior). Terminates when it receives the message "EXIT" and closes the
@@ -23,13 +28,39 @@ public class EchoReceiverThread extends Thread {
 			// loop untill exit received
 			try {
 				System.out.println(address + " wait for request ....");
+                                StringWriter out = new StringWriter();
+                                try{
+                                    JSONParser parser = new JSONParser();
+                                    Object obj = parser.parse(new FileReader("test.txt"));
+                                    JSONObject jsonObject = (JSONObject) obj;
+                                    System.out.print(obj);
+                                    jsonObject.writeJSONString(out);
+                                    
+                                    String name = (String) jsonObject.get("Name");
+                                    String author = (String) jsonObject.get("Author");
+                                    JSONArray companyList = (JSONArray) jsonObject.get("Company List");
+
+                                    System.out.println("Name: " + name);
+                                    System.out.println("Author: " + author);
+                                    System.out.println("\nCompany List:");
+                                    Iterator<String> iterator = companyList.iterator();
+                                    while (iterator.hasNext()) {
+                                        System.out.println(iterator.next());
+                                    }
+                                }
+                                catch(Exception e){
+                                    
+                                }
+                                
+                                //warte auf antwort
 				String request = tcpSocket.receiveLine();
-				String response = createResponseFor(request);
+				String response = createResponseFor(request) + out.toString();
 				if (response == null) {
 					tcpSocket.sendLine(EXIT);
 					tcpSocket.close();
 					tcpSocket = null; // cancels the loop
 				} else {
+                                        //antwort zurück
 					tcpSocket.sendLine(response);
 				}
 			} catch (IOException e) {
