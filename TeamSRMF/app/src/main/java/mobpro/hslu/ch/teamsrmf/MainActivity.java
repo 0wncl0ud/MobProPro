18b82b35f9c08c647c6b29803a1cfd9c86da13e7
Manuel Felber <manuel.felber@stud.hslu.ch>
2016-05-09 13:47:37 +0200

    manual merge

--- TeamSRMF/app/src/main/java/mobpro/hslu/ch/teamsrmf/MainActivity.java
+++ TeamSRMF/app/src/main/java/mobpro/hslu/ch/teamsrmf/MainActivity.java
@@ -3,14 +3,9 @@ package mobpro.hslu.ch.teamsrmf;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-<<<<<<< HEAD
 import android.view.View;
-=======
-import android.widget.TextView;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
->>>>>>> origin/master
+
 
 public class MainActivity extends AppCompatActivity {
 
